4827f575492fa1e2081832d57d1eed8acbbaec2f
Nodari-0 <nodarirusishvililinkedin@gmail.com>
2025-09-16 20:12:28 +0200

    Initial Supabase setup

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function SideBar() {
   return (
-    <div>SideBar</div>
-  )
+    <>
+      <div className="col-span-3 bg-bg p-4">
+        <h2 className="text-income text-xl font-bold mb-4">LOGO</h2>
+        <ul className="space-y-2">
+          <link className="text-[var(--color-navBar-text)] hover:text-white cursor-pointer">
+            Dashboard
+          </link>
+          <li className="text-[var(--color-navBar-text)] hover:text-white cursor-pointer">
+            Transactions
+          </li>
+          <li className="text-[var(--color-navBar-text)] hover:text-white cursor-pointer">
+            Settings
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
-export default SideBar
+export default SideBar;
